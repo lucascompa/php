3ba45025d47170974f6b34d0d59e90ac83e3c13a
lucascompa <lucascompa@gmail.com>
2017-05-13 18:19:55 -0300

    projeto

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-	echo "Ola mundo";
 	
+	require_once "vendor/autoload.php";
+
+	use \Slim\Slim;
+
+	$app = new \Slim\Slim();
+
+	$app->get('/hello/:name', function ($name) {
+	    echo "Hello, " . $name;
+	});
+	
+	$app->run();
+
 
 ?>
